8f2e1193b0b9e35cf8f77b0a19cc4465df2e703d
v9p49vz <xinyu.qu2016@gmail.com>
2021-01-09 11:42:17 +1300

    add base

---
+++ variables_study.py
@@ -0,0 +1,33 @@
+name = "Banana"
+dad = "Apple"
+number = 100
+my_mum_name = "Cherry"
+print(f"{name}")
+print(f"{dad}")
+print(f"{number}")
+print(f"I love {my_mum_name}")
+print(my_mum_name)
+
+frist_number = '10'
+second_number = '20'
+print(frist_number + second_number)
+
+frist_number = 10
+second_number = 20
+print(frist_number + second_number)
+
+print("My name is "+'lucas '+ "and 7 years old")
+frist_number = 45.2
+second_number = 4.9
+print(frist_number + second_number)
+
+# frist_number = '1000000000000000'
+# second_number = 45
+# print(frist_number + second_number)
+
+frist_number = 40
+second_number = 4.5
+print(frist_number + second_number)
+print(frist_number - second_number)
+print(frist_number * second_number)
+print(frist_number / second_number)
