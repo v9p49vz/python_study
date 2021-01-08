36c0bc43834e0420bca280057a7fa935055ce6a8
v9p49vz <xinyu.qu2016@gmail.com>
2021-01-09 12:34:03 +1300

    answer questions

--- variable_practice.py
+++ variable_practice.py
@@ -1,25 +1,40 @@
 # Q1:
 # define a variable: (type string, varialbe name: book, value: Harry Potter) and print the variable
-
+book = "Harry Potter"
+print(f"{book}")
+print(book)
 # Q2:
 # define a varible: (type integers, variable name: vol, value: 1) and print the variable
-
+vol = 1
+print(f"{vol}")
+print(vol)
 # Q3:
 # define a varible: (type float, variable name: float_number, value: 1.1) and print the variable
-
+float_number = 1.1
+print(f"{float_number}")
+print(float_number)
 # Q4:
 # define three string variables:
 # (1) name: sport name, value: foot_ball
 # (2) name: game name, value: chess
 # (3) name_ book name, value: Harry Potter
 # output: "I love foot_ball, chess and Harry Potter"
-
+sport = "foot_ball"
+game = "chess"
+book = "Harry Potter"
+print("I love " +sport +', '+ game+ " and "+book)
 # Q5:
 # Calculate the value of 55+99 and print the value
-
+first_number = 55
+second_number = 99
+print(first_number + second_number )
 # Q6:
 # Calculate the value of 4.5+5.8 and print the value
-
-
+first_number = 4.5
+second_number = 5.8
+print(first_number + second_number)
 # Q7:
 # Calculate the value of 24*5.6 and print the value
+first_number = 24
+second_number = 5.6
+print(first_number * second_number)
