7408887a00400f42fbc477f5464511362c35f46c
v9p49vz <xinyu.qu2016@gmail.com>
2021-01-09 11:55:42 +1300

    add questions

---
+++ variable_practice.py
@@ -0,0 +1,26 @@
+# Q1:
+# define a variable: (type string, varialbe name: book, value: Harry Potter) and print the variable
+
+# Q2:
+# define a varible: (type integers, variable name: vol, value: 1) and print the variable
+
+# Q3:
+# define a varible: (type float, variable name: float_number, value: 1.1) and print the variable
+
+# Q4:
+# define three string variables:
+# (1) name: sport name, value: foot_ball
+# (2) name: game name, value: chess
+# (3) name_ book name, value: Harry Potter
+# output: "I love foot_ball, chess and Harry Potter"
+
+# Q5:
+# Caculate the value of 55+99 and print the value
+
+# Q6:
+# caculate the value of 4.5+5.8 and print the value
+
+
+
+# Q7:
+# caculcate the value of 24*5.6 and print the value
